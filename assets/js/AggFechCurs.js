b896af9005e6d05f37f4c75273ea58b7bd2439ee
CarlosMi11 <163361644+CarlosMi11@users.noreply.github.com>
2024-03-27 11:37:18 -0400

    Add files via upload

---
+++ assets/js/AggFechCurs.js
@@ -0,0 +1,17 @@
+
+let cursoEC = document.getElementById("Info").dataset.curso;
+
+fetch('../assets/js/data.json')
+.then(response => response.json())
+.then(data => {
+
+    courses = data[cursoEC].cursosPresenciales;
+    let contenedor = document.getElementById("cursoPresencial");
+
+    courses.map(curso => {
+        contenedor.innerHTML+= `<p>curso ${curso.tipo.toUpperCase()}: ${curso.fecha} en ${curso.ubicacion}</p>`
+    })
+
+  
+})
+.catch(error => console.error('Error al cargar el JSON:', error));
