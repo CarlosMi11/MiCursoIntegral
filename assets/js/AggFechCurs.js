c26337114c5d49d9c51f45b6b243a802f657ac1e
CarlosMi11 <163361644+CarlosMi11@users.noreply.github.com>
2024-08-19 21:35:04 -0400

    Update AggFechCurs.js

--- assets/js/AggFechCurs.js
+++ assets/js/AggFechCurs.js
@@ -1,14 +1,14 @@
 
 let cursoEC = document.getElementById("Info").dataset.curso;
 
-fetch('../assets/js/data.json')
+fetch('../assets/js/cursos.json')
 .then(response => response.json())
 .then(data => {
 
     courses = data[cursoEC].cursosPresenciales;
     let contenedor = document.getElementById("cursoPresencial");
 
-    courses.map(curso => {
+    courses.fechas.map(curso => {
         contenedor.innerHTML+= `<p>Curso ${curso.tipo.toUpperCase()}: ${curso.fecha} en ${curso.ubicacion}</p>`
     })
 
